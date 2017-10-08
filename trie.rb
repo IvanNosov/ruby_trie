e725de3b5c387c9145e8accaca5e2c1cef7d60cb
Ivan Nosov <ivan7en@gmail.com>
2017-10-08 21:58:58 +0300

    trie

---
+++ trie.rb
@@ -0,0 +1,53 @@
+class Node
+  attr_accessor :med
+  attr_reader :data, :children
+  def initialize(data)
+    @data = data
+    @children = []
+    @med = false
+  end
+
+  def key?(key)
+    @children.each { |k| return true if k == key }
+    false
+  end
+
+  def search(key)
+    @children.each{ |k| return k if k == key}
+  end
+
+  def insert(key)
+    return search(key) if key? key
+    child = Node.new(key)
+    @children << child
+    child
+  end
+
+end
+
+class Trie
+  attr_reader :root
+  def initialize
+    @root = Node.new(nil)
+  end
+
+  def insert(word)
+    node = @root
+    word.to_s.each_char do |i|
+      child = node.insert(i)
+      node = child
+    end
+    node.med = true
+  end
+
+  def have?(word)
+    node = @root
+    word.each_char do |i|
+      return false unless node.key? i
+      node = node.search i
+    end
+
+    node.med
+  end
+end
+
